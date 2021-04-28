8919d6d99a51ab0f79af703f544041ebe368e9d5
Aleksei Belinskiy <escalion86@gmail.com>
2021-04-28 13:54:22 +0700

    Added components LessonCard & Stars

--- src/App.jsx
+++ src/App.jsx
@@ -7,93 +7,7 @@ import Avatar from './img/avatar.jpg'
 
 import SidePanel from './Pages/Blocks/SidePanel'
 import Header from './Pages/Blocks/Header'
-
-const Stars = ({ stars = 0, starstotal = 10, className = '' }) => {
-  const Star = ({ filled }) => (
-    <svg
-      class={'w-4 h-4 text-yellow-500' + (filled ? ' fill-current' : '')}
-      fill="none"
-      stroke="currentColor"
-      viewBox="0 0 24 24"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <path
-        stroke-linecap="round"
-        stroke-linejoin="round"
-        stroke-width="2"
-        d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z"
-      ></path>
-    </svg>
-  )
-  return (
-    <span
-      className={
-        'inline-flex items-center' + (className ? ' ' + className : '')
-      }
-    >
-      {[...Array(starstotal)].map((e, i) => (
-        <Star filled={i < stars}></Star>
-      ))}
-    </span>
-  )
-  // const card = _(5).times((idx) => <Star filled={idx < num}></Star>)
-  // return <Star filled />
-}
-
-const LessonCard = ({ name = 'Без названия', stars = 0, status = 0 }) => {
-  return (
-    <li className="">
-      <a href="#" className="block p-5 bg-white rounded-md shadow">
-        <div className="flex justify-between">
-          <p className="text-sm font-medium leading-snug text-gray-900">
-            {name}
-          </p>
-          <span>
-            <img className="h-6 w-6 rounded-full" src={Avatar} alt="" />
-          </span>
-        </div>
-        <div className="mt-2 flex items-center justify-between">
-          <div className="flex items-center">
-            <div className="text-sm text-gray-600">
-              {/* <time datetime="2019-09-14">Sep 14</time> */}
-              Сложность
-            </div>
-            <Stars className="pl-2" stars={stars} />
-          </div>
-
-          {/* <svg
-              className="h-2 w-2 text-blue-500"
-              viewBox="0 0 8 8"
-              fill="currentColor"
-            >
-              <circle cx="4" cy="4" r="3" />
-            </svg> */}
-          {status === 0 && (
-            <span className="px-2 py-1 leading-tight inline-flex items-center bg-red-100 rounded">
-              <span className="ml-1 text-sm font-medium text-red-700">
-                Не принят
-              </span>
-            </span>
-          )}
-          {status === 1 && (
-            <span className="px-2 py-1 leading-tight inline-flex items-center bg-yellow-100 rounded">
-              <span className="ml-1 text-sm font-medium text-yellow-600">
-                На проверке
-              </span>
-            </span>
-          )}
-          {status === 2 && (
-            <span className="px-2 py-1 leading-tight inline-flex items-center bg-green-100 rounded">
-              <span className="ml-1 text-sm font-medium text-green-700">
-                Выполнен
-              </span>
-            </span>
-          )}
-        </div>
-      </a>
-    </li>
-  )
-}
+import LessonCard from './Pages/Components/LessonCard'
 
 function App() {
   const menuCfg = [
@@ -101,7 +15,18 @@ function App() {
       name: 'Обучающие материалы',
       items: [
         { name: 'Видео уроки', href: '#', num: null, active: true },
-        { name: 'Онлайн уроки', href: '#', num: null, active: false },
+        {
+          name: 'Расписание живых уроков',
+          href: '#',
+          num: null,
+          active: false,
+        },
+        {
+          name: 'Расписание онлайн уроков',
+          href: '#',
+          num: null,
+          active: false,
+        },
       ],
     },
     {
@@ -122,21 +47,29 @@ function App() {
           Видео уроки
         </h2>
         <div className="items-center justify-between">
-          <main className="p-3">
+          <main className="p-3 space-y-3 ">
             <div className="p-3 bg-gray-100 rounded-md">
               <h3 className="text-base font-medium text-gray-900">Курс №1</h3>
               <ul className="space-y-2 mt-2">
-                <LessonCard name="Название фокуса" stars={4} status={0} />
-                <LessonCard name="Название фокуса" stars={8} status={1} />
-                <LessonCard name="Название фокуса" stars={9} status={2} />
-              </ul>
-            </div>
-            <div className="p-3 bg-gray-100 rounded-md">
-              <h3 className="text-base font-medium text-gray-900">Курс №1</h3>
-              <ul className="space-y-2 mt-2">
-                <LessonCard name="Название фокуса" stars={4} status={0} />
-                <LessonCard name="Название фокуса" stars={8} status={1} />
-                <LessonCard name="Название фокуса" stars={9} status={2} />
+                <LessonCard
+                  name="Название урока"
+                  description="Cillum qui cillum sit fugiat commodo dolore dolore voluptate excepteur velit duis sint labore in. Pariatur id dolor esse velit ipsum tempor. Et reprehenderit nisi dolore ut sint qui elit. Cupidatat cupidatat incididunt esse nisi exercitation proident commodo proident eu tempor. Minim cillum incididunt dolore est minim fugiat pariatur sit aliqua."
+                  stars={4}
+                  status={0}
+                  avatar={Avatar}
+                />
+                <LessonCard
+                  name="Название урока"
+                  description="Qui dolor nisi in magna laboris deserunt cillum proident sit tempor aliqua pariatur amet."
+                  stars={8}
+                  status={1}
+                />
+                <LessonCard
+                  name="Название урока"
+                  description="Описание урока"
+                  stars={9}
+                  status={2}
+                />
               </ul>
             </div>
           </main>
---
+++ src/Pages/Components/LessonCard.jsx
@@ -0,0 +1,74 @@
+import React from 'react'
+
+import Stars from './Stars'
+
+const LessonCard = ({
+  name = 'Без названия',
+  description = '',
+  stars = 0,
+  status = 0, // 0 - не начат, 1 - не принят, 2 - на проверке, 3 - выполнен
+  avatar = null,
+}) => {
+  return (
+    <li className="">
+      <a href="#" className="block px-5 py-4 bg-white rounded-md shadow">
+        <div className="flex justify-between items-center">
+          <p className="text-sm font-medium leading-snug text-gray-900">
+            {name}
+          </p>
+
+          <span className="h-6 w-6">
+            {avatar && (
+              <img className="h-6 w-6 rounded-full" src={avatar} alt="" />
+            )}
+          </span>
+        </div>
+        <div className="flex justify-between">
+          <p className="text-sm leading-tight text-gray-900 font-light">
+            {description}
+          </p>
+        </div>
+        <div className="mt-2 flex items-center justify-between">
+          <div className="flex items-center my-0.5">
+            <div className="text-sm text-gray-600">
+              {/* <time datetime="2019-09-14">Sep 14</time> */}
+              Сложность
+            </div>
+            <Stars className="pl-2" stars={stars} />
+          </div>
+
+          {/* <svg
+              className="h-2 w-2 text-blue-500"
+              viewBox="0 0 8 8"
+              fill="currentColor"
+            >
+              <circle cx="4" cy="4" r="3" />
+            </svg> */}
+          {status === 1 && (
+            <span className="px-2 py-0.5 leading-tight inline-flex items-center bg-red-100 rounded">
+              <span className="ml-1 text-sm font-medium text-red-700">
+                Не принят
+              </span>
+            </span>
+          )}
+          {status === 2 && (
+            <span className="px-2 py-0.5 leading-tight inline-flex items-center bg-yellow-100 rounded">
+              <span className="ml-1 text-sm font-medium text-yellow-600">
+                На проверке
+              </span>
+            </span>
+          )}
+          {status === 3 && (
+            <span className="px-2 py-0.5 leading-tight inline-flex items-center bg-green-100 rounded">
+              <span className="ml-1 text-sm font-medium text-green-700">
+                Выполнен
+              </span>
+            </span>
+          )}
+        </div>
+      </a>
+    </li>
+  )
+}
+
+export default LessonCard
