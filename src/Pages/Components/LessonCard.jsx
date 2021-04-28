f71d021ab8319e3b1571f47b314c2c739c12839e
Aleksei Belinskiy <escalion86@gmail.com>
2021-04-28 23:22:25 +0700

    Updated LessonCard

--- src/Pages/Components/LessonCard.jsx
+++ src/Pages/Components/LessonCard.jsx
@@ -6,7 +6,7 @@ const LessonCard = ({
   name = 'Без названия',
   description = '',
   stars = 0,
-  status = 0, // 0 - не начат, 1 - не принят, 2 - на проверке, 3 - выполнен
+  status = 0, // 0 - не доступен, 1 - доступен, но не начат, 2 - не принят, 3 - на проверке, 4 - выполнен
   avatar = null,
 }) => {
   return (
@@ -44,21 +44,28 @@ const LessonCard = ({
             >
               <circle cx="4" cy="4" r="3" />
             </svg> */}
-          {status === 1 && (
+          {status === 0 && (
+            <span className="px-2 py-0.5 leading-tight inline-flex items-center bg-gray-100 rounded">
+              <span className="ml-1 text-sm font-medium text-gray-400">
+                Не доступен
+              </span>
+            </span>
+          )}
+          {status === 2 && (
             <span className="px-2 py-0.5 leading-tight inline-flex items-center bg-red-100 rounded">
               <span className="ml-1 text-sm font-medium text-red-700">
                 Не принят
               </span>
             </span>
           )}
-          {status === 2 && (
+          {status === 3 && (
             <span className="px-2 py-0.5 leading-tight inline-flex items-center bg-yellow-100 rounded">
               <span className="ml-1 text-sm font-medium text-yellow-600">
                 На проверке
               </span>
             </span>
           )}
-          {status === 3 && (
+          {status === 4 && (
             <span className="px-2 py-0.5 leading-tight inline-flex items-center bg-green-100 rounded">
               <span className="ml-1 text-sm font-medium text-green-700">
                 Выполнен
