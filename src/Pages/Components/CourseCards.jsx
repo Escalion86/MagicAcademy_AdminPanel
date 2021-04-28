473cc6c82a90b4d6e1768c4bf4c1953eeda1f47e
Aleksei Belinskiy <escalion86@gmail.com>
2021-04-28 23:22:42 +0700

    Added CourseCards

---
+++ src/Pages/Components/CourseCards.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+import LessonCard from './LessonCard'
+
+const CourseCards = ({ course }) => {
+  if (typeof course !== 'object' || !course.name || !course.lessons) return null
+  return (
+    <div className="p-3 bg-gray-100 rounded-md">
+      <h3 className="text-base font-medium text-gray-700">{course.name}</h3>
+      <ul className="space-y-2 mt-2">
+        {course.lessons.map((lesson) => {
+          return (
+            <LessonCard
+              name={lesson.name}
+              description={lesson.description}
+              stars={lesson.stars}
+              status={lesson.status}
+              avatar={lesson.author.avatar}
+            />
+          )
+        })}
+      </ul>
+    </div>
+  )
+}
+
+export default CourseCards
