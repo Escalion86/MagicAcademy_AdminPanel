55b216cf0dcc71fe5b70b345d4af0726a9d3738b
Aleksei Belinskiy <escalion86@gmail.com>
2021-04-30 23:00:49 +0700

    CourseCards rename to Course

--- src/Pages/Components/Course.jsx
+++ src/Pages/Components/Course.jsx
@@ -2,15 +2,16 @@ import React from 'react'
 
 import LessonCard from './LessonCard'
 
-const CourseCards = ({ course }) => {
+const Course = ({ course }) => {
   if (typeof course !== 'object' || !course.name || !course.lessons) return null
   return (
     <div className="p-3 bg-gray-100 rounded-md">
       <h3 className="text-base font-medium text-gray-700">{course.name}</h3>
       <ul className="space-y-2 mt-2">
-        {course.lessons.map((lesson) => {
+        {course.lessons.map((lesson, index) => {
           return (
             <LessonCard
+              key={'lesson' + index}
               name={lesson.name}
               description={lesson.description}
               stars={lesson.stars}
@@ -24,4 +25,4 @@ const CourseCards = ({ course }) => {
   )
 }
 
-export default CourseCards
+export default Course
